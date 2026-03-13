1e7e00bef81a66c95db7a54d58c52e65d121ad3d
kawase <160330087+KawaseDev@users.noreply.github.com>
2026-03-13 21:02:51 +0200

    inited sprint boot services and added the application.properties

--- java-server/Java-Server/src/main/java/io/github/kawase/Server.java
+++ java-server/Java-Server/src/main/java/io/github/kawase/Server.java
@@ -2,9 +2,14 @@ package io.github.kawase;
 
 import io.github.kawase.client.Client;
 import io.github.kawase.client.ClientHandler;
+import io.github.kawase.database.services.ChildService;
+import io.github.kawase.database.services.GoalService;
+import io.github.kawase.database.services.ParentService;
+import io.github.kawase.database.services.TaskService;
 import io.github.kawase.packet.PacketManager;
 import io.github.kawase.socket.ServerSocket;
 import lombok.Getter;
+import org.springframework.context.ApplicationContext;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,9 +25,26 @@ public class Server {
     // we will init every manager here :pray:.
     private PacketManager packetManager = new PacketManager();
 
-    public void init(final int port) {
+    // sprint boot stuff.
+    private ApplicationContext context;
+
+    // services.
+    private ParentService parentService;
+    private TaskService taskService;
+    private ChildService childService;
+    private GoalService goalService;
+
+    public void init(final int port,  final ApplicationContext applicationContext) {
         this.packetManager = new PacketManager();
         this.activeConnections = new ConcurrentHashMap<>();
+
+        // init spring boot context.
+        this.context = applicationContext;
+
+        this.parentService = context.getBean(ParentService.class);
+        this.taskService = context.getBean(TaskService.class);
+        this.childService = context.getBean(ChildService.class);
+        this.goalService = context.getBean(GoalService.class);
 
         this.socket = new ServerSocket(port);
 
--- java-server/Java-Server/src/main/java/io/github/kawase/StartServer.java
+++ java-server/Java-Server/src/main/java/io/github/kawase/StartServer.java
@@ -1,13 +1,14 @@
 package io.github.kawase;
 
-import io.github.kawase.packet.Packet;
-import io.github.kawase.packet.PacketManager;
-import io.github.kawase.packet.impl.HandShakePacket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-import java.nio.ByteBuffer;
-
+@SpringBootApplication
 public class StartServer {
-    public static void main(String[] args) throws Exception {
-        Server.getInstance().init(69420);
+    public static void main(String[] args) {
+        final ApplicationContext context = SpringApplication.run(StartServer.class, args);
+
+        Server.getInstance().init(8887, context);
     }
 }
