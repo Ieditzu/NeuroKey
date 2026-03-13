307e493acf24db84c28cd6e1d5ce120add811c66
kawase <160330087+KawaseDev@users.noreply.github.com>
2026-03-13 20:20:58 +0200

    fix forgot to register the new packets

--- java-server/Java-Server/src/main/java/io/github/kawase/packet/PacketManager.java
+++ java-server/Java-Server/src/main/java/io/github/kawase/packet/PacketManager.java
@@ -1,14 +1,30 @@
 package io.github.kawase.packet;
 
 import io.github.kawase.exceptions.PacketException;
-import io.github.kawase.packet.impl.HandShakePacket;
+import io.github.kawase.packet.impl.*;
 
 public class PacketManager {
     public Packet createPacket(final int id) {
         return switch (id) {
             case 1 -> new HandShakePacket();
+            case 2 -> new AuthPacket();
+            case 3 -> new RegisterParentPacket();
+            case 4 -> new AddChildPacket();
+            case 5 -> new AddGoalPacket();
+            case 8 -> new CompleteTaskPacket();
+            case 9 -> new ActionResponsePacket();
+            case 10 -> new AuthResponsePacket();
+            case 11 -> new FetchTasksPacket();
+            case 12 -> new FetchTasksResponsePacket();
+            case 13 -> new FetchGoalsPacket();
+            case 14 -> new FetchGoalsResponsePacket();
+            case 15 -> new FetchChildrenPacket();
+            case 16 -> new FetchChildrenResponsePacket();
+            case 17 -> new FetchCompletedTasksPacket();
+            case 18 -> new FetchCompletedTasksResponsePacket();
 
             default -> throw new PacketException("Unknown packet ID: " + id);
         };
     }
 }
+
